6cf7c4351d9eeb479a0b837f0d25449fae275d5c
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-07-10 17:38:13 +0200

    Implement VIP filtering and re-modeling

---
+++ 22_remodel-vip-filtering.R
@@ -0,0 +1,43 @@
+################################################################################
+## Project:
+## Description:
+################################################################################
+
+## Return wavelengths with VIP > 1 =============================================
+
+wavelengths_vip_bigger1 <- df_vip_pls %>%
+  filter(vip > 1) %>%
+  pull(wavenumber)
+
+spc_train_model_vip_bigger1 <- select_spc_xvalues(
+  spc_tbl = spc_train_model, xvalues = wavelengths_vip_bigger1)
+
+
+## Return top 50 wavelengths with highest VIP ==================================
+
+wavelengths_vip_top50 <- df_vip_pls %>%
+  arrange(desc(vip)) %>%
+  .[1:50, ] %>%
+  pull(wavenumber)
+
+spc_train_model_vip_top50 <- select_spc_xvalues(
+  spc_tbl = spc_train_model, xvalues = wavelengths_vip_top50)
+
+
+## Remodel with VIP filtering ==================================================
+
+pls_starch_vip_bigger1 <- fit_pls(
+  spec_chem = spc_train_model_vip_bigger1,
+  response = starch,
+  evaluation_method = "resampling",
+  tuning_method = "resampling",
+  resampling_method = "rep_kfold_cv",  pls_ncomp_max = 10
+)
+
+pls_starch_vip_top50 <- fit_pls(
+  spec_chem = spc_train_model_vip_top50,
+  response = starch,
+  evaluation_method = "resampling",
+  tuning_method = "resampling",
+  resampling_method = "rep_kfold_cv",  pls_ncomp_max = 10
+)
