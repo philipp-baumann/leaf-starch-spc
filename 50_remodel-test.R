74dcc91b959ba5898276e7ea16473a436095c76f
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-07-17 16:13:49 +0200

    Re-calibrate with only test set data

---
+++ 50_remodel-test.R
@@ -0,0 +1,19 @@
+################################################################################
+## Project:
+## Description:
+################################################################################
+
+## Train a PLS regression model on the test set ================================
+
+seed_pls_starch_test <- set.seed(131L)
+
+pls_starch_test <- fit_pls(
+  spec_chem = spc_test_predict,
+  response = starch,
+  evaluation_method = "resampling",
+  tuning_method = "resampling",
+  resampling_method = "rep_kfold_cv", pls_ncomp_max = 10
+)
+
+pls_starch_test_rds <- readr::write_rds(x = pls_starch_test,
+  path = here("models", "pls_starch_test.Rds"))
