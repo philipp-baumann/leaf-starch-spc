47551a104993d00150f6a2b3c79d2e4d59e418a0
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-07-29 10:46:16 +0200

    Delete VIP filtering for training because not wrapped; implement regression line for ED vs. EN test evaluation

--- 40_predict-evaluate-train-test.R
+++ 40_predict-evaluate-train-test.R
@@ -61,20 +61,8 @@ p_training_self_eval <- ggplot(data = training_self_predobs,
 ## Extract cross-validated training predictions and do model evaluation ========
 
 training_eval_lm <- lm(pred ~ obs, data = pls_starch$predobs)
-training_eval_lm_vip_bigger1 <- lm(pred ~ obs,
-  data = pls_starch_vip_bigger1$predobs)
 
 training_eval <- pls_starch$stats %>%
-  # Modify columns for plot annotation
-  mutate(
-    rmse = as.character(as.expression(paste0("RMSE == ", "~",
-      "'", sprintf("%.0f", rmse), "'"))),
-    r2 = as.character(as.expression(paste0("italic(R)^2 == ", "~",
-      "'", sprintf("%.2f", r2), "'"))),
-    one_one = "1:1"
-  )
-
-training_eval_vip_bigger1 <- pls_starch_vip_bigger1$stats %>%
   # Modify columns for plot annotation
   mutate(
     rmse = as.character(as.expression(paste0("RMSE == ", "~",
@@ -119,40 +107,6 @@ p_training_eval <- pls_starch$predobs %>%
       panel.grid.minor = element_blank()
     )
 
-xyrange_training_vip_bigger1 <- xy_range(data = pls_starch_vip_bigger1$predobs,
-  x = obs, y = pred)
-
-p_training_eval_vip_bigger1 <- pls_starch_vip_bigger1$predobs %>%
-  mutate(eval_type = paste0("Training cross-validated (n = ", nrow(.), ")")
-  ) %>%
-  ggplot(data = , aes(x = obs, y = pred)) +
-    geom_point(alpha = 0.4) +
-    geom_abline(slope = 1) +
-    geom_abline(slope = training_eval_lm_vip_bigger1$coefficients[2],
-      intercept = training_eval_lm_vip_bigger1$coefficients[1], linetype = 2) +
-    coord_fixed(ratio = 1) +
-    facet_wrap(~ eval_type) +
-    geom_text(data = training_eval_vip_bigger1,
-      aes(x = Inf, y = -Inf, label = r2), size = 3,
-      hjust = 1.27, vjust = -3.5, parse = TRUE) +
-    geom_text(data = training_eval_vip_bigger1,
-      aes(x = Inf, y = -Inf, label = rmse), size = 3,
-      hjust = 1.08, vjust = -2.5, parse = TRUE) +
-    # xlab(expression(paste("Measured starch [", mg~g^-1, " DM]"))) +
-    xlab("") +
-    # ylab(expression(paste("Predicted starch [", mg~g^-1, " DM]"))) +
-    ylab("") +
-    xlim(xyrange_training_vip_bigger1[1] - 0.02 * diff(range(xyrange_training_vip_bigger1)),
-         xyrange_training_vip_bigger1[2] + 0.02 * diff(range(xyrange_training_vip_bigger1))) +
-    ylim(xyrange_training_vip_bigger1[1] - 0.02 * diff(range(xyrange_training_vip_bigger1)),
-         xyrange_training_vip_bigger1[2] + 0.02 * diff(range(xyrange_training_vip_bigger1))) +
-    theme_bw() +
-    theme(
-      strip.background = element_rect(colour = "black", fill = NA),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank()
-    )
-
 
 ## Combine training self-prediction and cross-validated training evaluation ====
 
@@ -178,24 +132,7 @@ test_predobs <- predict_from_spc(
   select(sample_id, sample_rep, harvest_time, starch, pls_starch) %>%
   mutate(eval_type = paste0("Test using full training (n = ", nrow(.), ")"))
 
-test_predobs_vip_bigger1 <- predict_from_spc(
-    model_list = list("pls_starch" = pls_starch_vip_bigger1),
-    spc_tbl = spc_test_predict_vip_bigger1) %>%
-  select(sample_id, sample_rep, harvest_time, starch, pls_starch) %>%
-  mutate(eval_type = paste0("Test using VIP training (n = ", nrow(.), ")"))
-
 test_eval <- evaluate_model(data = test_predobs, 
-  obs = starch, pred = pls_starch) %>% 
-  # Modify columns for plot annotation
-  mutate(
-    rmse = as.character(as.expression(paste0("RMSE == ", "~",
-      "'", sprintf("%.0f", rmse), "'"))),
-    r2 = as.character(as.expression(paste0("italic(R)^2 == ", "~",
-      "'", sprintf("%.2f", r2), "'"))),
-    one_one = "1:1"
-  )
-
-test_eval_vip_bigger1 <- evaluate_model(data = test_predobs_vip_bigger1, 
   obs = starch, pred = pls_starch) %>% 
   # Modify columns for plot annotation
   mutate(
@@ -245,58 +182,9 @@ p_test_eval <- ggplot(data = test_predobs, aes(x = starch, y = pls_starch)) +
     panel.grid.minor = element_blank()
   )
 
-p_test_eval_vip_bigger1 <- ggplot(data = test_predobs_vip_bigger1,
-    aes(x = starch, y = pls_starch)) +
-  geom_point(alpha = 0.4) +
-  geom_abline(slope = 1) +
-  geom_abline(slope = test_eval_lm_vip_bigger1$coefficients[2],
-    intercept = test_eval_lm_vip_bigger1$coefficients[1], linetype = 2) +
-  coord_fixed(ratio = 1) +
-  facet_wrap(~ eval_type) +
-  geom_text(data = test_eval_vip_bigger1,
-    aes(x = Inf, y = -Inf, label = r2), size = 3,
-      hjust = 1.27, vjust = -3.5, parse = TRUE) +
-  geom_text(data = test_eval_vip_bigger1,
-    aes(x = Inf, y = -Inf, label = rmse), size = 3,
-      hjust = 1.08, vjust = -2.5, parse = TRUE) +
-  # xlab(expression(paste("Measured starch [", mg~g^-1, " DM]"))) +
-  xlab("") +
-  ylab("") +
-  # ylab(expression(paste("Predicted starch [", mg~g^-1, " DM]"))) +
-  xlim(xyrange_test[1] - 0.02 * diff(range(xyrange_test)),
-       xyrange_test[2] + 0.02 * diff(range(xyrange_test))) +
-  ylim(xyrange_test[1] - 0.02 * diff(range(xyrange_test)),
-       xyrange_test[2] + 0.02 * diff(range(xyrange_test))) +
-  theme_bw() +
-  theme(
-    strip.background = element_rect(colour = "black", fill = NA),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()
-  )
-
 p_test_eval_pdf <- ggsave(filename = "test-eval.pdf", plot = p_test_eval,
   path = here("out", "figs"), width = 3, height = 3)
 
-p_test_eval_vip_bigger1_pdf <- ggsave(filename = "test-eval-vip-bigger1.pdf",
-  plot = p_test_eval_vip_bigger1, path = here("out", "figs"), 
-  width = 3, height = 3)
-
-
-## Combine test set evaluation for PLS training with all variables and 
-## re-calibrated training using VIP filtering ==================================
-
-p_eval_test_allvars_vip_bigger1 <- cowplot::plot_grid(
-  p_test_eval, p_test_eval_vip_bigger1,
-  ncol = 2) +
-  draw_label(expression(paste("Measured starch [", mg~g^-1, " DM]")), 
-    x = 0.55, y = 0, vjust = -0.3, angle = 0, size = 10) +
-  draw_label(expression(paste("Predicted starch [", mg~g^-1, " DM]")), 
-    x = 0, y = 0.5, vjust = 1.5, angle = 90, size = 10)
-
-p_eval_test_allvars_vip_bigger1_pdf <- ggsave(
-  filename = "test-eval-allvars-vip-bigger1.pdf",
-  plot = p_eval_test_allvars_vip_bigger1, path = here("out", "figs"), 
-  width = 6, height = 3)
 
 ## Combine cross-validated training and test set evaluation ====================
 
@@ -310,20 +198,8 @@ p_eval_training_test <- cowplot::plot_grid(p_training_eval, p_test_eval,
 p_eval_pdf <- ggsave(filename = "eval.pdf", plot = p_eval_training_test,
   path = here("out", "figs"), width = 6, height = 3)
 
-p_eval_training_test_vip_bigger1 <- cowplot::plot_grid(
-  p_training_eval_vip_bigger1, p_test_eval_vip_bigger1,
-  ncol = 2) +
-  draw_label(expression(paste("Measured starch [", mg~g^-1, " DM]")), 
-    x = 0.55, y = 0, vjust = -0.3, angle = 0, size = 10) +
-  draw_label(expression(paste("Predicted starch [", mg~g^-1, " DM]")), 
-    x = 0, y = 0.5, vjust = 1.5, angle = 90, size = 10)
 
-p_eval_vip_bigger1_pdf <- ggsave(filename = "eval-vip-bigger1.pdf",
-  plot = p_eval_training_test_vip_bigger1, path = here("out", "figs"),
-  width = 6, height = 3)
-
-
-## Test evaluation depicted by genotype ========================================
+## Test evaluation grouped by genotype =========================================
 
 # New genotype column
 test_predobs_genotype <- test_predobs %>%
@@ -379,5 +255,152 @@ p_test_predobs_genotype <-
 p_test_predobs_genotype_pdf <- ggsave(
   filename = "predobs-test-genotype.pdf",
   plot = p_test_predobs_genotype, path = here("out", "figs"),
+  width = 7, height = 2.5)
+
+
+## Test evaluation grouped by measurement time 
+## (end of night (EN) vs. end of day (ED)) =====================================
+
+# Model evaluation by harvest time
+test_eval_harvest_time <- test_predobs %>%
+  split(.$harvest_time) %>%
+  map(~ evaluate_model(data = ., obs = starch, pred = pls_starch)) %>%
+  imap(~ tibble::add_column(.x, harvest_time = .y, .before = 1)) %>%
+  bind_rows() %>%
+  # Modify columns for plot annotation
+  mutate(
+    rmse = as.character(as.expression(paste0("RMSE == ", "~",
+      "'", sprintf("%.0f", rmse), "'"))),
+    r2 = as.character(as.expression(paste0("italic(R)^2 == ", "~",
+      "'", sprintf("%.2f", r2), "'"))),
+    one_one = "1:1"
+  )
+
+regline_test_harvest <- test_predobs_genotype %>%
+  split(.$harvest_time) %>%
+  map(~ lm(pls_starch ~ starch, data = .x)) %>%
+  map(broom::tidy) %>%
+  imap(~ add_column(.x, harvest_time = .y, .before = 1)) %>%
+  map(~ tidyr::spread(.x, term, estimate) %>%
+    rename(int = `(Intercept)`, slope = starch)) %>%
+  map(~ list(
+    int = pull(.x, int) %>% .[!is.na(.)],
+    slope = pull(.x, slope) %>% .[!is.na(.)]
+    )
+  ) %>%
+  imap(~ as_tibble(.x) %>% add_column(harvest_time = .y, .before = 1)) %>%
+  bind_rows()
+
+p_test_predobs_harvest_time <-
+  test_predobs %>%
+  mutate(genotype = map_chr(sample_rep,
+    ~ stringr::str_replace(.x, pattern = "_[[:digit:]]+", replacement = ""))
+  ) %>%
+  ggplot(aes(x = starch, y = pls_starch), data = .) +
+  geom_abline(slope = 1, colour = "black") +
+  geom_abline(data = regline_test_harvest, aes(intercept = int, slope = slope),
+    colour = "black", linetype = 2) +
+  geom_point(aes(colour = genotype, shape = genotype), alpha = 0.7) +
+  # scale_colour_manual(values = c("#d7191c", "#2b83ba")) +
+  facet_wrap(~ harvest_time) +
+  geom_text(data = test_eval_harvest_time,
+      aes(x = Inf, y = -Inf, label = r2), size = 3,
+      hjust = 1.27, vjust = -2.75, parse = TRUE) +
+  geom_text(data = test_eval_harvest_time,
+      aes(x = Inf, y = -Inf, label = rmse), size = 3,
+      hjust = 1.08, vjust = -1.75, parse = TRUE) +
+  coord_fixed(ratio = 1) +
+  xlim(xyrange_test[1] - 0.02 * diff(range(xyrange_test)),
+       xyrange_test[2] + 0.02 * diff(range(xyrange_test))) +
+  ylim(xyrange_test[1] - 0.02 * diff(range(xyrange_test)),
+       xyrange_test[2] + 0.02 * diff(range(xyrange_test))) +
+  xlab(expression(paste("Measured starch [mg ", g^{-1}, " DW]"))) +
+  ylab(expression(paste("Predicted starch [mg ", g^{-1}, " DW]"))) +
+  labs(colour = "Genotype", shape = "Genotype") +
+  theme_bw() +
+  theme(
+    strip.background = element_rect(colour = "black", fill = NA),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+p_test_predobs_harvest_time_pdf <- ggsave(
+  filename = "predobs-test-genotype-harvest-time.pdf",
+  plot = p_test_predobs_harvest_time, path = here("out", "figs"),
+  width = 7, height = 2.5)
+
+
+## Training evaluation grouped by measurement time
+## (end of night (EN) vs. end of day (ED)) =====================================
+
+# Augument predictions with metadata
+train_predobs_meta <- spc_train_model %>%
+  select(sample_id, sample_age_rep, harvest_time, genotype, leaf_age, rep) %>%
+  inner_join(x = ., y = pls_starch$predobs)
+
+training_eval_harvest_time <- train_predobs_meta %>%
+  # mutate(genotype = str_replace(genotype, pattern = ".[[:digit:]]", "")) %>%
+  split(.$harvest_time) %>%
+  map(~ evaluate_model(data = ., obs = obs, pred = pred)) %>%
+  imap(~ tibble::add_column(.x, harvest_time = .y, .before = 1)) %>%
+  bind_rows() %>%
+  # Modify columns for plot annotation
+  mutate(
+    rmse = as.character(as.expression(paste0("RMSE == ", "~",
+      "'", sprintf("%.0f", rmse), "'"))),
+    r2 = as.character(as.expression(paste0("italic(R)^2 == ", "~",
+      "'", sprintf("%.2f", r2), "'"))),
+    one_one = "1:1"
+  )
+
+regline_train_harvest <- train_predobs_meta %>%
+  split(.$harvest_time) %>%
+  map(~ lm(pred ~ obs, data = .x)) %>%
+  map(broom::tidy) %>%
+  imap(~ add_column(.x, harvest_time = .y, .before = 1)) %>%
+  map(~ tidyr::spread(.x, term, estimate) %>%
+    rename(int = `(Intercept)`, slope = obs)) %>%
+  map(~ list(
+    int = pull(.x, int) %>% .[!is.na(.)],
+    slope = pull(.x, slope) %>% .[!is.na(.)]
+    )
+  ) %>%
+  imap(~ as_tibble(.x) %>% add_column(harvest_time = .y, .before = 1)) %>%
+  bind_rows()
+
+p_training_predobs_harvest_time <-
+  train_predobs_meta %>%
+  ggplot(aes(x = obs, y = pred), data = .) +
+  geom_abline(slope = 1, colour = "black") +
+  geom_abline(data = regline_train_harvest, aes(intercept = int, slope = slope),
+    colour = "black") +
+  geom_point(aes(colour = leaf_age, shape = leaf_age), alpha = 0.5) +
+  # scale_colour_manual(values = c("#d7191c", "#2b83ba")) +
+  facet_wrap(~ harvest_time) +
+  geom_text(data = training_eval_harvest_time,
+      aes(x = Inf, y = -Inf, label = r2), size = 3,
+      hjust = 1.27, vjust = -2.75, parse = TRUE) +
+  geom_text(data = training_eval_harvest_time,
+      aes(x = Inf, y = -Inf, label = rmse), size = 3,
+      hjust = 1.08, vjust = -1.75, parse = TRUE) +
+  coord_fixed(ratio = 1) +
+  xlim(xyrange_training[1] - 0.02 * diff(range(xyrange_training)),
+       xyrange_training[2] + 0.02 * diff(range(xyrange_training))) +
+  ylim(xyrange_training[1] - 0.02 * diff(range(xyrange_training)),
+       xyrange_training[2] + 0.02 * diff(range(xyrange_training))) +
+  xlab(expression(paste("Measured starch [mg ", g^{-1}, " DW]"))) +
+  ylab(expression(paste("Predicted starch [mg ", g^{-1}, " DW]"))) +
+  # scale_shape_manual(values = c(0, 1, 2)) + 
+  labs(colour = "Leaf age", shape = "Leaf age") +
+  theme_bw() +
+  theme(
+    strip.background = element_rect(colour = "black", fill = NA),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  )
+
+p_training_predobs_harvest_time_pdf <- ggsave(
+  filename = "predobs-training-harvest-time.pdf",
+  plot = p_training_predobs_harvest_time, path = here("out", "figs"),
   width = 7, height = 2.5)
 
