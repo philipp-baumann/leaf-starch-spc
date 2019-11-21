3eba4d465d400a9434671ea72d3e646efc7e7401
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2019-11-21 08:27:40 +0100

    Simplify drake template

--- 00_setup-make.R
+++ 00_setup-make.R
@@ -30,42 +30,8 @@ plans <- map(rlang::set_names(scripts), code_to_plan)
 ## Define and make the plan ====================================================
 
 # Set up parallization
-plan(multicore)
+plan(multiprocess)
 registerDoFuture()
-
-# Modified from example in section "future":
-# https://ropensci.github.io/drake/articles/parallelism.html
-
-future_plans <- list(
-  eval_seq = future::sequential,
-  eval_par = future::multicore
-)
-evaluator <- NULL
-
-make_evaluator <- function(plan, future_plan, evaluator = NULL) {
-  for (i in seq_len(nrow(plan))) {
-    evaluator <- c(evaluator, future_plan)
-  }
-  evaluator
-}
-
-# Make the targets with the multisession future backend...
-evaluator <- map(
-  plans, 
-  ~ make_evaluator(plan = .x, future_plan = future_plans$eval_par))
-
-# Specify explicitly which scripts are sequential
-rows_eval_seq <- c("")
-
-for (i in rows_eval_seq) {
-  evaluator[[i]] <- make_evaluator(plan = plans[[i]],
-    future_plan = future_plans$eval_seq)
-}
-
-# Add evalutor columns for each sub-plan to specify future strategy
-for (i in names(plans)) {
-  plans[[i]]$evaluator <- evaluator[[i]]
-}
 
 ## Finalize into one master drake plan, make configuration, and run workflow ===
 
@@ -74,6 +40,17 @@ plan <- bind_rows(
   plans
 )
 
+## Build targets (R objects) listed in plan ====================================
 
-make(plan, parallelism = "future", jobs = 48, lazy_load = TRUE,
-  keep_going = TRUE, lock_envir = FALSE, memory_strategy = "speed")
+# Record start time
+start_time <- Sys.time()
+make(plan, lock_envir = FALSE)
+end_time <- Sys.time()
+
+exec_time <- end_time - start_time
+
+# Predict runtime to rebuild the entire drake cache
+predict_runtime(config, from_scratch = TRUE)
+
+# make(plan, parallelism = "future", jobs = 48, lazy_load = TRUE,
+#   keep_going = TRUE, lock_envir = FALSE, memory_strategy = "speed")
