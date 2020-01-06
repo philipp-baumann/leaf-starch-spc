c255c49026f8316159366237b9f20cabcd5075f2
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-01-06 18:49:02 +0100

    Script renamed

--- 00_setup-make.R
+++ 00_setup-make.R
@@ -35,7 +35,7 @@ scripts <- c(
   "50_remodel-test.R",
   "51_interpret-test-vip.R",
   "52_remodel-test-vip-training.R",
-  "60_evaluate-remodel-test.R"
+  "60_evaluate-test.R"
 )
 
 plans <- map(rlang::set_names(scripts), code_to_plan)
