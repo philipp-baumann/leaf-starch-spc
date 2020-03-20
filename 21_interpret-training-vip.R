f79ef425cbb077a2f8ce12bb58c5fdf993a55151
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-03-20 10:37:23 +0100

    Add PLSR coefficient panel; resize panels

--- 21_interpret-training-vip.R
+++ 21_interpret-training-vip.R
@@ -54,21 +54,8 @@ wl_peaks <- wl[peaks]
 # Merge soil component abbreviations to peaks
 constituents <- rep("", length(wl_peaks))
 
-# constituents[1] <- " O-H (K)"
-# constituents[3] <- " O-H (K)"
-# constituents[4] <- " Alkyl C-H"
-# constituents[5] <- " Alkyl C-H"
-# constituents[10:13] <- rep(" Si-O (Q)", 4)
-# constituents[22] <- " Al-OH (K)"
-# constituents[23] <- " Al-OH (K)"
-
 annotation_const <- paste0(round(wl_peaks, 0), constituents, sep = "")
 
-# df_peak <- tibble(
-#   wavenumber = wl_peaks,
-#   annotation = annotation_const,
-#   y_peaks = spc_max_peaks
-# )
 
 ## Plot spectra and VIP scores =================================================
 
@@ -118,14 +105,8 @@ p_spc <- ggplot(dts_long[["spc"]],
   geom_line(data = dts_long[["spc"]], inherit.aes = FALSE,
     aes(x = wavelength, y = value, group = group_id),
       alpha = alpha, size = 0.2) +
-  # geom_vline(data = df_peak, aes(xintercept = wavenumber), color = "#e41a1c",
-  #   linetype = "dotted", size = 0.4, alpha = 0.6) +
-  # geom_text_repel(data = df_peak, aes(x = wavenumber, y = y_peaks, 
-  #   label = annotation_const), size = 2, angle = 90, col = "#e41a1c", 
-  #   fontface = "bold", arrow = arrow(length = unit(0.02, "npc")),
-  #   direction = "both", fill = "white", nudge_y = 0.45) +
   scale_x_continuous(limits = x_lim, breaks = brk) +
-  ylim(c(-0.1, 0.6)) +
+  ylim(c(-0.05, 0.52)) +
   labs(x = xlab, y = ylab1) +
   theme_bw() +
   theme(
@@ -150,8 +131,7 @@ p_spc_pre <- ggplot(dts_long[["spc_pre"]],
   scale_x_continuous(limits = x_lim, breaks = brk) +
   ylim(c(-0.009, 0.013)) +
   theme(
-    plot.margin = unit(c(1, 5, -25, 6),
-    units = "points"),
+    plot.margin = unit(c(1, 5, -25, 6), units = "points"),
     axis.title.y = element_text(vjust = 0.25),
     axis.text.x = element_blank())
 
@@ -165,27 +145,46 @@ p_vip <- ggplot(data = df_vip_pls,
     fill = "orange", alpha = 0.3) +
   geom_hline(yintercept = 1, colour = "black") +
   geom_line(aes(colour = model), size = 0.55) +
-  geom_point(aes(x = wavenumber, y = coef_bigger0),
-    alpha = 1/5, size = 0.7, colour = "black", shape = 124) +
   xlab(xlab) +
   ylab("VIP") +
   scale_x_continuous(limits = x_lim, breaks = brk) +
   theme_bw() +
+  theme(
+    plot.margin = unit(c(1, 5, -23, 6), units = "points"),
+    axis.title.y = element_text(vjust = 0.25),
+    axis.text.x = element_blank(),
+    legend.position = "none"
+  )
+
+# Plot PLS regression coefficients ---------------------------------------------
+
+p_coef <- ggplot(data = df_vip_pls,
+    aes(x = wavenumber, y = coef)) +
+  geom_rect(data = rects, inherit.aes = FALSE,
+    aes(xmin = start, xmax = end, ymin = min(df_vip_pls$coef),
+    ymax = max(df_vip_pls$coef), group = group), color = "transparent",
+    fill = "orange", alpha = 0.3) +
+  geom_hline(yintercept = 0, colour = "black") +
+  geom_line(aes(colour = model), size = 0.55) +
+  xlab(xlab) +
+  ylab(expression(paste("PLSR coef. (", italic(b), ")"))) +
+  scale_x_continuous(limits = x_lim, breaks = brk) +
+  theme_bw() +
   theme(plot.margin = unit(c(0, 5, 1, 1),
     units = "points"), axis.title.y = element_text(vjust = 0.25),
-    legend.position = "bottom") +
-  guides(colour = guide_legend(title = "Model/outcome"))
+    legend.position = "none") # +
+  # guides(colour = guide_legend(title = "Model/outcome"))
 
 # Arrange plots in panels without margins --------------------------------------
 # No margins in between
 
 p_vip_comb <- cowplot::plot_grid(
-  p_spc, p_spc_pre, p_vip, rel_heights = c(0.35, 0.3, 0.6),
+  p_spc, p_spc_pre, p_vip, p_coef, rel_heights = c(0.35, 0.3, 0.2, 0.3),
   ncol = 1, align = "v")
 
 p_vip_comb_pdf <- ggsave(filename = "spc-starch-pls-vip.pdf", plot = p_vip_comb, 
-  path = here("out", "figs"), width = 6.69, height = 5)
+  path = here("out", "figs"), width = 6.69, height = 5.25)
 
 p_vip_comb_pdf_pub <- ggsave(filename = "S4.pdf",
   plot = p_vip_comb, 
-  path = here("pub", "figs"), width = 6.69, height = 5)
+  path = here("pub", "figs"), width = 6.69, height = 5.25)
