add5d1740b8f8ae714ebc52545354f9d49bc337e
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-03-20 13:45:00 +0100

    Add panel with PLSR coefficients; resize panels

--- 51_interpret-test-vip.R
+++ 51_interpret-test-vip.R
@@ -49,22 +49,9 @@ wl_peaks_test <- wl_test[peaks_test]
 # Merge soil component abbreviations to peaks
 constituents_test <- rep("", length(wl_peaks_test))
 
-# constituents_test[1] <- " O-H (K)"
-# constituents_test[3] <- " O-H (K)"
-# constituents_test[4] <- " Alkyl C-H"
-# constituents_test[5] <- " Alkyl C-H"
-# constituents_test[10:13] <- rep(" Si-O (Q)", 4)
-# constituents_test[22] <- " Al-OH (K)"
-# constituents_test[23] <- " Al-OH (K)"
-
 annotation_const_test <- paste0(
   round(wl_peaks_test, 0), constituents_test, sep = "")
 
-# df_peak_test <- tibble(
-#   wavenumber = wl_peaks_test,
-#   annotation = annotation_const_test,
-#   y_peaks = spc_max_peaks_test
-# )
 
 ## Plot spectra and VIP scores =================================================
 
@@ -79,7 +66,7 @@ brk_vip_test <- pretty(
   as.numeric(names(dts_test[["spc"]])[!names(dts_test[["spc"]]) %in% 
     c("spc_id", "lcol_type", "group_id")])
 )
-# minmax <- function(x) {c(min(x), max(x))} # see helpers
+
 x_lim_test <- minmax(
   as.numeric(names(dts_test[["spc"]])[!names(dts_test[["spc"]]) %in%
     c("spc_id", "lcol_type", "group_id")])
@@ -110,14 +97,8 @@ p_spc_test <- ggplot(dts_test_long[["spc"]],
   geom_line(data = dts_test_long[["spc"]], inherit.aes = FALSE,
     aes(x = wavelength, y = value, group = group_id),
       alpha = alpha_vip_test, size = 0.2) +
-  # geom_vline(data = df_peak, aes(xintercept = wavenumber), color = "#e41a1c",
-  #   linetype = "dotted", size = 0.4, alpha = 0.6) +
-  # geom_text_repel(data = df_peak, aes(x = wavenumber, y = y_peaks, 
-  #   label = annotation_const), size = 2, angle = 90, col = "#e41a1c", 
-  #   fontface = "bold", arrow = arrow(length = unit(0.02, "npc")),
-  #   direction = "both", fill = "white", nudge_y = 0.45) +
   scale_x_continuous(limits = x_lim_test, breaks = brk_vip_test) +
-  ylim(c(-0.1, 0.6)) +
+  ylim(c(-0.05, 0.55)) +
   labs(x = xlab_vip_test, y = ylab1_vip_test) +
   theme_bw() +
   theme(
@@ -157,28 +138,49 @@ p_vip_test <- ggplot(data = df_vip_pls_test,
     fill = "orange", alpha = 0.3) +
   geom_hline(yintercept = 1, colour = "black") +
   geom_line(aes(colour = model), size = 0.55) +
-  geom_point(aes(x = wavenumber, y = coef_bigger0),
-    alpha = 1/5, size = 0.7, colour = "black", shape = 124) +
-  xlab(xlab_vip_test) +
+  ylim(c(min(df_vip_pls_test$vip) - 0.3, max(df_vip_pls_test$vip))) +
+  xlab("") +
   ylab("VIP") +
   scale_x_continuous(limits = x_lim_test, breaks = brk_vip_test) +
   theme_bw() +
-  theme(plot.margin = unit(c(0, 5, 1, 1),
-    units = "points"), axis.title.y = element_text(vjust = 0.25),
-    legend.position = "bottom") +
-  guides(colour = guide_legend(title = "Model/outcome"))
+  theme(
+    plot.margin = unit(c(1, 5, -23, 6), units = "points"), # unit(c(0, 5, 1, 1)
+    axis.title.y = element_text(vjust = 0.25),
+    axis.text.x = element_blank(),
+    legend.position = "none"
+  )
+
+# Plot PLS regression coefficients ---------------------------------------------
+
+p_coef_test <- ggplot(data = df_vip_pls_test,
+    aes(x = wavenumber, y = coef)) +
+  geom_rect(data = rects_test, inherit.aes = FALSE,
+    aes(xmin = start, xmax = end, ymin = min(df_vip_pls_test$coef),
+    ymax = max(df_vip_pls_test$coef), group = group), color = "transparent",
+    fill = "orange", alpha = 0.3) +
+  geom_hline(yintercept = 0, colour = "black") +
+  geom_line(aes(colour = model), size = 0.55) +
+  xlab(xlab_vip_test) +
+  ylab(expression(paste("PLSR coef. (", italic(b), ")"))) +
+  scale_x_continuous(limits = x_lim_test, breaks = brk_vip_test) +
+  theme_bw() +
+  theme(
+    plot.margin = unit(c(0, 5, 1, 1), units = "points"), 
+    axis.title.y = element_text(vjust = 0.25),
+    legend.position = "none"
+  )
 
 # Arrange plots in panels without margins --------------------------------------
 # No margins in between
 
 p_vip_test_comb <- cowplot::plot_grid(
-  p_spc_test, p_spc_pre_test, p_vip_test, rel_heights = c(0.35, 0.3, 0.6),
-  ncol = 1, align = "v")
+  p_spc_test, p_spc_pre_test, p_vip_test, p_coef_test,
+  rel_heights = c(0.35, 0.3, 0.2, 0.3), ncol = 1, align = "v")
 
 p_vip_test_comb_pdf <- ggsave(filename = "spc-starch-pls-test-vip.pdf",
   plot = p_vip_test_comb, 
-  path = here("out", "figs"), width = 6.5, height = 5)
+  path = here("out", "figs"), width = 6.69, height = 5.25)
 
 p_vip_test_comb_pdf_pub <- ggsave(filename = "S5.pdf",
   plot = p_vip_test_comb, 
-  path = here("pub", "figs"), width = 6.69, height = 5)
+  path = here("pub", "figs"), width = 6.69, height = 5.25)
