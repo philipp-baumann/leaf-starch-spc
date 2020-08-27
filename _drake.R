3b13c0683dad325f79e57151d3b933008c225a62
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2020-08-27 17:30:42 +0200

    Drake make: Be cristal clear for function calls

--- _drake.R
+++ _drake.R
@@ -8,18 +8,18 @@
 source("packages.R")
 
 # Functions
-sourceDirectory("R")
+R.utils::sourceDirectory("R")
 
 # Script-based workflow: convert to drake plan; 
 # all assigned R objects in scripts are drake targets
-scripts <- listDirectory("code", fullNames = TRUE)
+scripts <- R.utils::listDirectory("code", fullNames = TRUE)
 plans <- lapply(setNames(x = scripts, nm = scripts), drake::code_to_plan)
 
 ## Define and make the plan ====================================================
 
 # Set up parallization
-plan(multiprocess)
-registerDoFuture()
+future::plan(future::multiprocess)
+doFuture::registerDoFuture()
 
 ## Finalize into one master drake plan, make configuration, and run workflow ===
 
@@ -36,7 +36,7 @@ plan <- do.call(rbind, plans)
 drake::make(plan, cache_log_file = TRUE, lock_envir = FALSE)
 
 # Predict runtime to rebuild the entire drake cache
-predict_runtime(config, from_scratch = TRUE)
+drake::predict_runtime(config, from_scratch = TRUE)
 
 # make(plan, parallelism = "future", jobs = 48, lazy_load = TRUE,
 #   keep_going = TRUE, lock_envir = FALSE, memory_strategy = "speed")
